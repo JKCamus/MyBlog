a97aaeb7bb43b909cf140ee0cc9d56d21aed9004
JKCamus <924850758@qq.com>
2020-12-11 10:48:02 +0800

    去掉topLayer,在app灌入isMobile

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,13 +1,37 @@
-import React, { memo } from "react";
+import React, { memo,useEffect } from "react";
+
+import { HashRouter } from "react-router-dom";
+import routes from "./router";
+import { renderRoutes } from "react-router-config";
+
 import { Provider } from "react-redux";
 import store from "./store";
+import { changeIsMobileAction } from "./store/global/actionCreators";
+// 动效库，用于判断是否是手机
+import { Skeleton } from "antd";
+import { enquireScreen } from "enquire-js";
 
-import TopLayer from "./TopLayer";
+import Footer from "components/app-footer/Footer";
+import Header from "components/app-header/Header";
+
 
 export default memo(function App() {
+  useEffect(() => {
+    enquireScreen((b) => {
+      b
+        ? store.dispatch(changeIsMobileAction(b))
+        : store.dispatch(changeIsMobileAction(false))
+    });
+  }, []);
   return (
     <Provider store={store}>
-      <TopLayer></TopLayer>
+      <HashRouter>
+        <Skeleton loading={false} active paragraph={{ rows: 5 }}>
+          <Header></Header>
+          {renderRoutes(routes)}
+          <Footer></Footer>
+        </Skeleton>
+      </HashRouter>
     </Provider>
   );
 });

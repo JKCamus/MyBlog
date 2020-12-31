3e271f873fd086392b2371fa3c4b228c2a9b9ec2
JKCamus <924850758@qq.com>
2020-12-31 09:28:50 +0800

    一个猜想

--- my-blog/src/components/app-header/Header.jsx
+++ my-blog/src/components/app-header/Header.jsx
@@ -3,11 +3,14 @@ import { useDispatch, useSelector, shallowEqual } from "react-redux";
 import TweenOne from "rc-tween-one";
 import { NavLink } from "react-router-dom";
 import "./index.less";
-import { Avatar } from "antd";
+import { Avatar, Menu, Dropdown } from "antd";
+
 import { UserOutlined } from "@ant-design/icons";
 import checkLogin from "utils/checkLogin";
 
 const Header = (props) => {
+  const { Item, SubMenu } = Menu;
+
   const { isMobile } = useSelector((state) => ({
     isMobile: state.getIn(["global", "isMobile"]),
     shallowEqual,
@@ -22,16 +25,31 @@ const Header = (props) => {
     // console.log('setShowLogin',login )
   };
 
+  const menu = (
+    <Menu theme="dark">
+      <Menu.Item className={"dropDownMenu"}>
+        <NavLink
+          to={"/profile"}
+          // onClick={() => setPhoneOpen(!phoneOpen)}
+        >
+          Profile
+        </NavLink>
+      </Menu.Item>
+      <Menu.Item className={"dropDownMenu"}>
+        <span>Logout</span>
+      </Menu.Item>
+    </Menu>
+  );
   return (
     <TweenOne
       component="header"
       animation={{ opacity: 0, type: "from" }}
-      className={"header2 home-page-wrapper"}
+      className={"header home-page-wrapper"}
     >
       <div className={`home-page${phoneOpen ? " open" : ""}`}>
         <TweenOne
           animation={{ x: -30, type: "from", ease: "easeOutQuad" }}
-          className={"header2-logo"}
+          className={"header-logo"}
         >
           <NavLink
             to={"/"}
@@ -45,15 +63,9 @@ const Header = (props) => {
             CamusBlog
           </NavLink>
         </TweenOne>
-
-        {/* {!isMobile && (
-          <div className={"avatar"}>
-            <Avatar size={36} icon={<UserOutlined />} />
-          </div>
-        )} */}
         {isMobile && (
           <div
-            className={"header2-mobile-menu"}
+            className={"header-mobile-menu"}
             onClick={() => {
               setPhoneOpen(!phoneOpen);
             }}
@@ -64,7 +76,7 @@ const Header = (props) => {
           </div>
         )}
         <TweenOne
-          className={"header2-menu"}
+          className={"header-menu"}
           animation={
             isMobile
               ? {
@@ -83,6 +95,62 @@ const Header = (props) => {
           reverse={!!phoneOpen}
         >
           {!checkLogin() ? (
+            <span className={"menu-item"} onClick={handleLogin}>
+              Login
+            </span>
+          ) : (
+            // <Dropdown
+            //   overlay={menu}
+            //   // overlayClassName={"avatarStyle"}
+            //   theme="dark"
+            //   trigger={["click"]}
+            //   className={"menu-item"}
+            //   // overlayStyle={{backgroundColor:'red'}}
+            // >
+            //   <div>
+            //     <Avatar size={36} onClick={(e) => e.preventDefault()} />
+            //   </div>
+            // </Dropdown>
+            <Menu
+              theme="dark"
+              className={`${isMobile ? "" : "menu-item"}`}
+              mode={isMobile ? "inline" : "horizontal"}
+              // overflowedIndicator={<UserOutlined />}
+            >
+              <SubMenu
+                key="sub1"
+                // title="Navigation One"
+                icon={<UserOutlined />}
+              >
+                <Menu.Item key="1">Option 1</Menu.Item>
+                <Menu.Item key="2">Option 2</Menu.Item>
+              </SubMenu>
+            </Menu>
+          )}
+
+          <NavLink
+            to={"/demo"}
+            className={"menu-item"}
+            onClick={() => setPhoneOpen(!phoneOpen)}
+          >
+            Demo
+          </NavLink>
+          <NavLink
+            to={"/charts"}
+            className={"menu-item"}
+            onClick={() => setPhoneOpen(!phoneOpen)}
+          >
+            Charts
+          </NavLink>
+          <NavLink
+            to={"/profile"}
+            className={"menu-item"}
+            onClick={() => setPhoneOpen(!phoneOpen)}
+          >
+            Profile
+          </NavLink>
+
+          {/* {!checkLogin() ? (
             <span className={"menu-item"} onClick={handleLogin}>
               Login
             </span>
@@ -112,7 +180,7 @@ const Header = (props) => {
             onClick={() => setPhoneOpen(!phoneOpen)}
           >
             Profile
-          </NavLink>
+          </NavLink> */}
         </TweenOne>
       </div>
     </TweenOne>

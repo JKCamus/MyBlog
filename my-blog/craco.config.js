e058687d5365d3c1b11cb320bc4e6ac3e1f75c39
JKCamus <924850758@qq.com>
2021-01-15 16:53:34 +0800

    暂放

--- my-blog/craco.config.js
+++ my-blog/craco.config.js
@@ -9,11 +9,24 @@ process.env.GENERATE_SOURCEMAP = "false";
 // 添加webpack速度分析,在webpack那边假如smp
 const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
 const smp = new SpeedMeasurePlugin();
+// 编译进度条
+const WebpackBar = require("webpackbar");
+// 压缩js
+const TerserPlugin = require("terser-webpack-plugin");
+// 打包信息配置
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+// 开启gzip
+const CompressionWebpackPlugin = require("compression-webpack-plugin");
 
 const { POSTCSS_MODES, whenProd } = require("@craco/craco");
 
 const threadLoader = require("thread-loader");
-const WebpackBar = require("webpackbar");
+
+const jsWorkerPool = {
+  workers: 2,
+  poolTimeout: 2000,
+};
+threadLoader.warmup(jsWorkerPool, ["babel-loader"]);
 
 module.exports = {
   webpack: smp.wrap({
@@ -26,58 +39,109 @@ module.exports = {
       store: resolve("src/store"),
       utils: resolve("src/utils"),
     },
-    //抽离公用模块
-    optimization: {
-      splitChunks: {
-        cacheGroups: {
-          commons: {
-            chunks: "initial",
-            minChunks: 2,
-            maxInitialRequests: 5,
-            minSize: 0,
+
+    configure: {
+      /*在这里添加任何webpack配置选项: https://webpack.js.org/configuration */
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: [
+              {
+                loader: "thread-loader",
+                options: jsWorkerPool,
+              },
+              // "babel-loader?cacheDirectory",
+            ],
           },
-          vendor: {
-            test: /node_modules/,
-            chunks: "initial",
-            name: "vendor",
-            priority: 10,
-            enforce: true,
+        ],
+      },
+      resolve: {
+        modules: [
+          // 指定以下目录寻找第三方模块，避免webpack往父级目录递归搜索
+          resolve("src"),
+          resolve("node_modules"),
+        ],
+        alias: {
+          "@": resolve("src"), // 缓存src目录为@符号，避免重复寻址
+        },
+      },
+      //抽离公用模块
+      optimization: {
+        minimize: process.env.REACT_APP_ENV !== "development" ? true : false,
+        splitChunks: {
+          chunks: "all", // initial、async和all
+          minSize: 30000, // 形成一个新代码块最小的体积
+          maxAsyncRequests: 5, // 按需加载时候最大的并行请求数
+          maxInitialRequests: 3, // 最大初始化请求数
+          automaticNameDelimiter: "~", // 打包分割符
+          name: true,
+          cacheGroups: {
+            // commons: {
+            //   chunks: "initial",
+            //   minChunks: 2,
+            //   maxInitialRequests: 5,
+            //   minSize: 0,
+            // },
+            // vendor: {
+            //   test: /node_modules/,
+            //   chunks: "initial",
+            //   name: "vendor",
+            //   priority: 10,
+            //   enforce: true,
+            // },
+            "async-commons": {
+              // 其余异步加载包
+              chunks: "async",
+              minChunks: 2,
+              name: "async-commons",
+              priority: 90,
+            },
+            commons: {
+              // 其余同步加载包
+              chunks: "all",
+              minChunks: 2,
+              name: "commons",
+              priority: 80,
+            },
           },
         },
       },
     },
+
     plugins: [
       // webpack进度条
       new WebpackBar({ color: "green", profile: true }),
       // 打包时，启动插件
-      // ...whenProd(
-      //   () => [
-      //     // 压缩js 同时删除console debug等
-      //     new TerserPlugin({
-      //       parallel: true, // 多线程
-      //       terserOptions: {
-      //         ie8: true,
-      //         // 删除注释
-      //         output: {
-      //           comments: false,
-      //         },
-      //         //删除console 和 debugger  删除警告
-      //         compress: {
-      //           drop_debugger: true,
-      //           drop_console: true,
-      //         },
-      //       },
-      //     }),
-      //     // 开启gzip
-      //     new CompressionWebpackPlugin({
-      //       // 是否删除源文件，默认: false
-      //       deleteOriginalAssets: false,
-      //     }),
-      //     // 打包分析
-      //     new BundleAnalyzerPlugin(),
-      //   ],
-      //   []
-      // ),
+      ...whenProd(
+        () => [
+          // 压缩js 同时删除console debug等
+          new TerserPlugin({
+            // parallel: true, // 多线程
+            // terserOptions: {
+            //   ie8: true,
+            //   // 删除注释
+            //   output: {
+            //     comments: false,
+            //   },
+            //   //删除console 和 debugger  删除警告
+            //   compress: {
+            //     drop_debugger: true,
+            //     drop_console: true,
+            //   },
+            // },
+          }),
+          // 开启gzip
+          // new CompressionWebpackPlugin({
+          //   // 是否删除源文件，默认: false
+          //   deleteOriginalAssets: false,
+          // }),
+          // 打包分析
+          new BundleAnalyzerPlugin(),
+        ],
+        []
+      ),
     ],
   }),
   // 插件相关配置，一个plugin一个对象
@@ -88,7 +152,7 @@ module.exports = {
       plugin: CracoLessPlugin,
       options: {
         lessLoaderOptions: {
-          // 用于配置主题或者说是标志颜色
+          // 配置主题或者说是标志颜色
           lessOptions: {
             // modifyVars: { "@primary-color": "#1DA57A" },
             modifyVars: { "@primary-color": "#43a3ef" },

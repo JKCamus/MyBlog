571d26148c7a81aae4090d99a11d87d4cd272eb2
JKCamus <924850758@qq.com>
2021-06-08 23:10:56 +0800

    feat：useDebounce

--- my-blog/src/utils/index.ts
+++ my-blog/src/utils/index.ts
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 /*
  * @Description:
@@ -6,10 +6,29 @@ import { useEffect } from "react";
  * @Author: camus
  * @Date: 2021-06-08 22:37:50
  * @LastEditors: camus
- * @LastEditTime: 2021-06-08 22:40:58
+ * @LastEditTime: 2021-06-08 23:09:11
+ */
+/**
+ * @description: mounted hook，mounted的时候回调，更新不回调
+ * @param {function} callback
  */
 export const useMount = (callback: () => void) => {
   useEffect(() => {
     callback();
   }, []);
+};
+/**
+ * @description: useDebounce 防抖hook
+ * @param {any} value
+ * @param {number} delay
+ */
+export const useDebounce = (value: any, delay: number) => {
+  const [debounceValue, setDebounceValue] = useState(value);
+  useEffect(() => {
+    const timeout = setTimeout(() => setDebounceValue(value), delay);
+    return () => {
+      return () => clearTimeout(timeout);
+    };
+  }, [value, delay]);
+  return debounceValue;
 };

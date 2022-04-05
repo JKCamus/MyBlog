cddf2302c572ba947820af97527689f9ed302687
JKCamus <924850758@qq.com>
2022-04-05 17:57:44 +0800

    feat: test

--- my-blog/src/components/app-footer/TS/index.ts
+++ my-blog/src/components/app-footer/TS/index.ts
@@ -4,8 +4,12 @@
  * @Author: camus
  * @Date: 2022-02-07 20:00:09
  * @LastEditors: camus
- * @LastEditTime: 2022-02-07 20:01:44
+ * @LastEditTime: 2022-03-27 16:45:05
  */
 export type MyPick<T,K extends keyof T>={
   [key in K]:T[key]
 }
+
+
+// 根据 Github 用户名获取用户信息
+// function calculateDaysBetweenDates(begin, end) {

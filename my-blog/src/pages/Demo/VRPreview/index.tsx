43b4e3bd043ae72cdc0024d0b2562b09ddae64ca
JKcamus <924850758@qq.com>
2023-11-01 14:38:49 +0800

    feat: little planet effect test

--- my-blog/src/pages/Demo/VRPreview/index.tsx
+++ my-blog/src/pages/Demo/VRPreview/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
+// panorama 全景
 const VRPreview: React.FC = (props) => {
   const [isPANOLENSLoaded, setPANOLENSLoaded] = useState(false);
   const [isThreeLoaded, setIsThreeLoaded] = useState(false);
@@ -11,26 +12,28 @@ const VRPreview: React.FC = (props) => {
       await import("./panolens.min.js");
       setPANOLENSLoaded(true);
     };
-    loadSource()
+    loadSource();
   }, []);
 
   useEffect(() => {
     if (isPANOLENSLoaded && isThreeLoaded) {
-
       const container = document.getElementById("container");
-
+      // console.log('three', THREE)
       const viewer = new PANOLENS.Viewer({
         container: container,
         controlButtons: ["fullscreen", "setting", "video"],
-        autoRotate: true, //自动播放
+        autoRotate: false, //自动播放
         autoRotateActivationDuration: 2000, //时长
         autoRotateSpeed: 0.2, //速度
       });
 
+      //   const panorama = new PANOLENS.ImageLittlePlanet(
+      //     "src/assets/img/Photo_6553761_DJI_161_pano_12007956_0_2023719574_photo_original.JPG"
+      // );
       const panorama = new PANOLENS.ImagePanorama(
         "src/assets/img/Photo_6553761_DJI_161_pano_12007956_0_2023719574_photo_original.JPG"
       );
-
+      // console.log('viwer', viewer)
       viewer.add(panorama);
     }
   }, [isPANOLENSLoaded, isThreeLoaded]);

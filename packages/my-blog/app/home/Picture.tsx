e1306c1a7c519cf328fdea1896823a2013499c56
JKCamus <924850758@qq.com>
2024-01-31 09:42:53 +0800

    feat(deps): 添加antd,配置antd

--- packages/my-blog/app/home/Picture.tsx
+++ packages/my-blog/app/home/Picture.tsx
@@ -1,7 +1,43 @@
-
+import { Button, Image, Tag, Timeline } from 'antd'
 const Picture = () => {
   return (
-    <div>Picture</div>
+    <div>
+      Picture
+      <Button type="primary">Button</Button>
+      <Tag color="magenta">
+      <a href="https://github.com/ant-design/ant-design/issues/1862">Link</a>
+    </Tag>
+    <Image.PreviewGroup
+    items={[
+      'https://gw.alipayobjects.com/zos/antfincdn/LlvErxo8H9/photo-1503185912284-5271ff81b9a8.webp',
+      'https://gw.alipayobjects.com/zos/antfincdn/cV16ZqzMjW/photo-1473091540282-9b846e7965e3.webp',
+      'https://gw.alipayobjects.com/zos/antfincdn/x43I27A55%26/photo-1438109491414-7198515b166b.webp',
+    ]}
+  >
+    <Image
+      width={200}
+      src="https://gw.alipayobjects.com/zos/antfincdn/LlvErxo8H9/photo-1503185912284-5271ff81b9a8.webp"
+    />
+  </Image.PreviewGroup>
+
+    <Timeline
+    items={[
+      {
+        children: 'Create a services site 2015-09-01',
+      },
+      {
+        children: 'Solve initial network problems 2015-09-01',
+      },
+      {
+        children: 'Technical testing 2015-09-01',
+      },
+      {
+        children: 'Network problems being solved 2015-09-01',
+      },
+    ]}
+  />
+
+    </div>
   )
 }
 

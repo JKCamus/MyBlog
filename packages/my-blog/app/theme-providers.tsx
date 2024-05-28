ecd014d50c11b34918493a0fce710b96da9c1d2d
JKCamus <924850758@qq.com>
2024-05-28 15:08:17 +0800

    feat(components): 暂用主题方案

--- packages/my-blog/app/theme-providers.tsx
+++ packages/my-blog/app/theme-providers.tsx
@@ -2,7 +2,7 @@
 
 import siteMetadata from '@/data/siteMetadata'
 import { ThemeProvider } from 'next-themes'
-import ThemeAntdProviders from './them-antd-provider'
+import ThemeAntdProviders from './theme-antd-provider'
 export function ThemeProviders({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme} enableSystem>
